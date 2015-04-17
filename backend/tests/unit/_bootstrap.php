83d1c6247579bb87be6306b269b102a147d04ded
jawo <jakub@wojtyra.it>
2015-04-18 01:31:46 +0200

    add codeception

---
+++ backend/tests/unit/_bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+include_once __DIR__."/../../../vendor/autoload.php";
+include_once __DIR__."/../../../vendor/yiisoft/yii2/Yii.php";
+
+//define('YII_ENV', 'test');
+//
+//$kernel = AspectMock\Kernel::getInstance();
+//$kernel->init([
+////    'debug' => true,
+//    'includePaths' => [__DIR__.'/../../../common',__DIR__.'/../../../backend/modules'],
+//    'cacheDir'  => '/tmp/myapp',
+//]);
